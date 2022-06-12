59110e12d9b27835f71d7035338b85662e7ebac8
Peter-McKinney <45462076+Peter-McKinney@users.noreply.github.com>
2022-06-12 05:47:20 -0500

    Replace mocha with jest

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -5,15 +5,14 @@ import {
   parseParadeOrg,
   removeHtml,
 } from "../src/index";
-import { expect } from "chai";
 
 describe("isOrg", () => {
   it("should return true if org string", function () {
-    expect(isOrg("12:00 noon")).to.be.true;
+    expect(isOrg("12:00 noon")).toBe(true);
   });
 
   it("should return false if not org string", () => {
-    expect(isOrg("lksdflkjf <")).to.be.false;
+    expect(isOrg("lksdflkjf <")).toBe(false);
   });
 });
 
@@ -22,7 +21,7 @@ describe("removeHtml", () => {
     const html = "<body><p>hello</p></body>";
     const sanitizedString = removeHtml(html);
 
-    expect(sanitizedString).to.be.equal("hello");
+    expect(sanitizedString).toBe("hello");
   });
 });
 
@@ -30,7 +29,7 @@ describe("parseParadeDate", () => {
   it("should parse date", () => {
     const html = "<body><p><strong>SUNDAY, FEBRUARY 20</strong></p></body>";
     const expectedDate = new Date("2/20/2022");
-    expect(parseParadeDate(html).toDateString()).to.equal(
+    expect(parseParadeDate(html).toDateString()).toBe(
       expectedDate.toDateString()
     );
   });
@@ -42,8 +41,8 @@ describe("parseParadeOrg", () => {
       "<body><p><a>7:00 pm - Order of Butterfly Maidens Parade</a></p></body>";
     const org = parseParadeOrg(html);
 
-    expect(org[0]).to.be.equal("Order of Butterfly Maidens Parade");
-    expect(org[1]).to.be.equal("7:00 pm");
+    expect(org[1]).toBe("7:00 pm");
+    expect(org[0]).toBe("Order of Butterfly Maidens Parade");
   });
 });
 
@@ -53,5 +52,5 @@ describe("combineDateTime", () => {
   const expectedDate = new Date("3/5/2022 19:30:00Z");
 
   const combinedDate = combineDateTime(date, time);
-  expect(combinedDate.toString()).to.be.equal(expectedDate.toString());
+  expect(combinedDate.toString()).toBe(expectedDate.toString());
 });
